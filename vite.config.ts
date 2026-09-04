253b47e12b393c71cd5838a126b5fe1c2fa76454
Maurice Lang <106346768+MauLang18@users.noreply.github.com>
2026-09-03 18:35:06 -0600

    fix(pricing): restore exact 7ba LCL wizard transform pipeline

--- vite.config.ts
+++ vite.config.ts
@@ -7,24 +7,16 @@ import { pricingLclSourceVisibilityFix } from './build/pricingLclSourceVisibilit
 import { pricingLclCostBreakdownUi } from './build/pricingLclCostBreakdownUi'
 import { pricingWizardFixedAutomaticCostEditFix } from './build/pricingWizardFixedAutomaticCostEditFix'
 import { pricingWizardEnhancementsScoped } from './build/pricingWizardEnhancementsScoped'
-import { pricingWizardOwnLclLinePersistence } from './build/pricingWizardOwnLclLinePersistence'
 import { pricingWizardLclRouteContextFix } from './build/pricingWizardLclRouteContextFix'
 import { pricingWizardUiParity } from './build/pricingWizardUiParity'
 import { pricingWizardLclFclParityFix } from './build/pricingWizardLclFclParityFix'
-import { pricingWizardLclStableFlow } from './build/pricingWizardLclStableFlow'
 import { pricingWizardSep02Requirements } from './build/pricingWizardSep02Requirements'
 import { pricingWizardLclOptionalWeight } from './build/pricingWizardLclOptionalWeight'
 import { pricingWizardLclFinalGuard } from './build/pricingWizardLclFinalGuard'
 import { pricingWizardScreen09LclFix } from './build/pricingWizardScreen09LclFix'
-import { pricingWizardAgentCountryFilter } from './build/pricingWizardAgentCountryFilter'
-import { pricingWizardOwnLclExcelOnly } from './build/pricingWizardOwnLclExcelOnly'
-import { pricingSellerRateRequests } from './build/pricingSellerRateRequests'
-import { pricingSellerScopeAccess } from './build/pricingSellerScopeAccess'
-import { pricingWizardToastErrors } from './build/pricingWizardToastErrors'
-import { pricingWizardLclSourceSummary } from './build/pricingWizardLclSourceSummary'
 
 export default defineConfig({
-  plugins: [pricingWizardLclOptionalWeight(), pricingWizardSep02Requirements(), pricingWizardLclCorrections(), pricingLclSourceVisibilityFix(), pricingLclCostBreakdownUi(), pricingWizardFixedAutomaticCostEditFix(), pricingWizardEnhancementsScoped(), pricingWizardOwnLclLinePersistence(), pricingWizardLclRouteContextFix(), pricingWizardUiParity(), pricingWizardLclFclParityFix(), pricingWizardLclStableFlow(), pricingWizardLclFinalGuard(), pricingWizardScreen09LclFix(), pricingWizardAgentCountryFilter(), pricingWizardOwnLclExcelOnly(), pricingSellerRateRequests(), pricingSellerScopeAccess(), pricingWizardToastErrors(), pricingWizardLclSourceSummary(), vue(), tailwindcss()],
+  plugins: [pricingWizardLclOptionalWeight(), pricingWizardSep02Requirements(), pricingWizardLclCorrections(), pricingLclSourceVisibilityFix(), pricingLclCostBreakdownUi(), pricingWizardFixedAutomaticCostEditFix(), pricingWizardEnhancementsScoped(), pricingWizardLclRouteContextFix(), pricingWizardUiParity(), pricingWizardLclFclParityFix(), pricingWizardLclFinalGuard(), pricingWizardScreen09LclFix(), vue(), tailwindcss()],
 
   resolve: {
     alias: {
